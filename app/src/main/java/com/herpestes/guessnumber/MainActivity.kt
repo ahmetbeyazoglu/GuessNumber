e7c0985fd474d57f0abbc0135b2c811bd461fde3
herpestes <ahmetbeyazoglu@gmail.com>
2023-07-03 20:44:15 +0300

    Design

--- app/src/main/java/com/herpestes/guessnumber/MainActivity.kt
+++ app/src/main/java/com/herpestes/guessnumber/MainActivity.kt
@@ -3,12 +3,18 @@ package com.herpestes.guessnumber
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.herpestes.guessnumber.ui.theme.GuessNumberTheme
 
@@ -29,13 +35,23 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Composable
-fun mainPage(){
+fun mainPage() {
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.SpaceEvenly) {
+        Text(text = "Guess Game")
+        Image(painter = painterResource(id = R.drawable.zar_resim), contentDescription = null)
+        Button(onClick = { }) {
+            Text(text = "Start Game")
+
+        }
+
+    }
 
 }
 
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     GuessNumberTheme {
